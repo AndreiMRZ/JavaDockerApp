4096c6f193aa7c1a43efe80cfa2ca9407b7bd247
AndreiMRZ <tudor_andrei2011@yahoo.com>
2023-08-23 16:19:13 +0300

    msg

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,4 @@
+ResourceGroup = "ResourceGroup"
+location = "West Europe"
+cluster_name = "aks-terraform-cluster"
+kubernetes_version = "1.27.2"
---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "ResourceGroup" {
+  type = string
+  description = "resource group name"
+}
+
+variable "location" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "kubernetes_version" {
+  type = string
+}
